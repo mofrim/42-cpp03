822a6d40733aec97410d36a3eb66933ce15d256b
mofrim <mofrim@posteo.de>
2025-07-24 14:21:00 +0200

    fine-code ex01

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 20:42:42 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/07/21 10:57:41 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/07/24 14:15:02 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@
 
 class ClapTrap
 {
+  // gotta make this protected, not private. otherwise ScavTrap wouldn't be able
+  // to access theses members.
   protected:
     ClapTrap();
 
     std::string _name;
-    std::string _prefix;
+    std::string _msgPrefix;
     long        _hp;
     long        _energy;
     int         _dmg;
@@ -32,9 +34,9 @@ class ClapTrap
     ClapTrap& operator=(const ClapTrap& other);
     ~ClapTrap();
 
-    virtual void beRepaired(unsigned int amount);
-    virtual void takeDamage(unsigned int amount);
-    virtual void attack(const std::string& target);
+    void beRepaired(const unsigned int& amount);
+    void takeDamage(const unsigned int& amount);
+    void attack(const std::string& target);
 };
 
 #endif
